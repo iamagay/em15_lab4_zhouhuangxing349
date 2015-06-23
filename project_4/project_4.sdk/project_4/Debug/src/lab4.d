c224191392d4ac68f12052ff56846736bbf47bb6
iamagay <836707412@qq.com>
2015-06-23 16:24:42 +0800

    lab4
    lab4

---
+++ project_4/project_4.sdk/project_4/Debug/src/lab4.d
@@ -0,0 +1,51 @@
+src/lab4.o src/lab4.d: ../src/lab4.c \
+ ../../project_4_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../project_4_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../project_4_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../project_4_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../project_4_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../project_4_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../project_4_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../project_4_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../project_4_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../project_4_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../project_4_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../project_4_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../project_4_bsp/ps7_cortexa9_0/include/xparameters.h ../src/xutil.h \
+ ../../project_4_bsp/ps7_cortexa9_0/include/xbasic_types.h \
+ ../../project_4_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../project_4_bsp/ps7_cortexa9_0/include/led_ip.h
+
+../../project_4_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../project_4_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../project_4_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../project_4_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../project_4_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../project_4_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../project_4_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../project_4_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../project_4_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../project_4_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../project_4_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../project_4_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../project_4_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../src/xutil.h:
+
+../../project_4_bsp/ps7_cortexa9_0/include/xbasic_types.h:
+
+../../project_4_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../project_4_bsp/ps7_cortexa9_0/include/led_ip.h:
